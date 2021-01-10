707696d61a3a067741a79a41cc7bf92f752da4e7
KallDrexx <me@mshapiro.net>
2021-01-10 11:45:36 -0500

    Added benchmark project

---
+++ JsonGenerators.Benchmarks/HardCodedBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
+
+namespace JsonGenerators.Benchmarks
+{
+    [SimpleJob()]
+    public class HardCodedBenchmark
+    {
+        private const string Json = @"{""StringValue"":""abcd efg"",""IntValue"":23,""DoubleValue"":3.45,""BoolValue"":true}";
+
+        [Benchmark]
+        public TestClass JsonDotNet() => JsonConvert.DeserializeObject<TestClass>(Json);
+
+        [Benchmark]
+        public TestClass CustomDeserializer() => TestClassDeserializer.Deserialize(Json);
+    }
+}
---
+++ JsonGenerators.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace JsonGenerators.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
